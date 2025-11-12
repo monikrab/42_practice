2ab5bc2cc8044108f6b211ae4bf3186c4f8bdcf6
monikrab <monikrab@yandex.com>
2025-11-12 09:54:42 +0000

    finished strsrt

--- c03/ft_strstr.c
+++ c03/ft_strstr.c
@@ -3,29 +3,36 @@ char*		ft_strstr(char* str, char* to_find)
 {
 	int i = 0, j = 0;
 
-	while (str[i] != to_find[j]) i++;
+	if (to_find[0] == '\0') return str; // String empty
 
-	
-	while (1)
+	while (str[i] != '\0')
 	{
-		if (str[i] == to_find[j])
+		if (to_find[j] == str[i]) i++, j++; // Char in common, inc both
+
+		else if (to_find[j] == '\0') // Check if finished iterating over to_find
 		{
-			if (str[i] != '\0')
+			while (str[i])
 			{
-				to_find[j] = str[i];
+				to_find[j] = str[i]; // Fill to_find with the rest of str after its first occurence
+				i++, j++;
 			}
-			j++; i++;
 		}
-		else return to_find;
+
+		else if (i > 0 && to_find[j - 1] == str[i - 1]) i++, j = 0; // Reset to_find to 0 if incomplete string found
+
+		else i++; // No char in common, leave to_find at 0
 	}
-	return to_find;
+
+	if (str[i] == to_find[j]) return to_find; // If both ended on \0, return to_find and exit
+
+	return NULL; // String not found
 }
 
 
-/* int main()
-{
-	char s1[] = "Madonna Madowwa";
-	char s2[30] = "dowwa";
+// int main()
+// {
+// 	char s1[] = "Madow Mawod";
+// 	char s2[30] = "wod";
 
-	printf("%s", ft_strstr(s1, s2));
-} */
+// 	printf("%s", ft_strstr(s1, s2));
+// } 
